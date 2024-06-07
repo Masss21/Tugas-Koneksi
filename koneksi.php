654e17b462546485b098ade53fd44d64b22a9464
Masss21 <dimasaryokumboro@gmail.com>
2024-06-07 14:03:48 +0700

    koneksi.php

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,11 @@
 <?php
 $host = "localhost";
 $user = "root";
-$password = "";
-$database = "uts_312310015";
+$pass = "";
+$db = "uts_312310015";
 
-// Membuat koneksi ke database
-$koneksi = mysqli_connect($host, $user, $password, $database);
+$koneksi = mysqli_connect($host, $user, $pass, $db);
 
-// Cek koneksi
 if (!$koneksi) {
     die("Koneksi database gagal: " . mysqli_connect_error());
 }
